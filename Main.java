844e0489e619241c09b5c11e30d297aca2609aa0
Furkan Safa Altunyuva <furkansafaaltunyuva@gmail.com>
2023-02-23 01:53:44 +0300

    New Upload

--- Main.java
+++ Main.java
@@ -2,39 +2,38 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
         ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
 
         ArrayList<Integer> list1 = new ArrayList<>();
         ArrayList<Integer> list2 = new ArrayList<>();
         ArrayList<Integer> list3 = new ArrayList<>();
         ArrayList<Integer> list4 = new ArrayList<>();
 
-        ArrayList<Integer> evenNumbers = new ArrayList<>();
-        ArrayList<Integer> oddNumbers = new ArrayList<>();
-
-        Thread t1 = new Thread();
-        Thread t2 = new Thread();
-        Thread t3 = new Thread();
-        Thread t4 = new Thread();
-
-        for(int i = 1; i<=10000; i++){
-            if(i<=250){
+        for (int i = 1; i <= 10000; i++) {
+            if (i <= 2500) {
                 list1.add(i);
-            }
-            else if (i>250 && i<=500) {
+            } else if (i > 2500 && i <= 5000) {
                 list2.add(i);
-            }
-            else if (i>500 && i<=750) {
+            } else if (i > 5000 && i <= 7500) {
                 list3.add(i);
-            }
-            else{
+            } else {
                 list4.add(i);
             }
 
             list.add(i);
         }
 
+        ThreadOverride t1 = new ThreadOverride(list1, evenNumbers, oddNumbers);
+        ThreadOverride t2 = new ThreadOverride(list2, evenNumbers, oddNumbers);
+        ThreadOverride t3 = new ThreadOverride(list3, evenNumbers, oddNumbers);
+        ThreadOverride t4 = new ThreadOverride(list4, evenNumbers, oddNumbers);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
 
 
     }
--- ThreadOverride.java
+++ ThreadOverride.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
 
 public class ThreadOverride extends Thread {
+    public ArrayList<Integer> list;
+    public ArrayList<Integer> evenNumbers;
+    public ArrayList<Integer> oddNumbers;
+
+    public ThreadOverride(ArrayList<Integer> list, ArrayList<Integer> evenNumbers, ArrayList<Integer> oddNumbers) {
+        this.list = list;
+        this.evenNumbers = evenNumbers;
+        this.oddNumbers = oddNumbers;
+    }
 
     @Override
-    public void run(ArrayList<Integer> list, ArrayList<Integer> evenNumbers, ArrayList<Integer> oddNumbers) {
-        for(Integer value: list){
-            if(value % 2 == 0)
+    public void run() { //ArrayList<Integer> list, ArrayList<Integer> evenNumbers, ArrayList<Integer> oddNumbers
+        for (Integer value : list) {
+            if (value % 2 == 0)
                 evenNumbers.add(value);
             else
                 oddNumbers.add(value);
